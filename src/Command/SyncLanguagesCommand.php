c6445e69239e940713067475f2e3754c5752a7a8
Filip Kowalik <filip.kowalik@strix.net>
2022-06-20 13:22:31 +0200

    SWERG-8: sync languages from ergonode

---
+++ src/Command/SyncLanguagesCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Strix\Ergonode\Command;
+
+use Shopware\Core\Framework\Api\Context\SystemSource;
+use Shopware\Core\Framework\Context;
+use Strix\Ergonode\Manager\LanguageManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class SyncLanguagesCommand extends Command
+{
+    protected static $defaultName = 'strix:ergonode:languages:sync';
+
+    private Context $context;
+
+    private LanguageManager $languageManager;
+
+    public function __construct(
+        LanguageManager $languageManager
+    ) {
+        parent::__construct();
+
+        $this->context = new Context(new SystemSource());
+        $this->languageManager = $languageManager;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription(
+            'Fetches all languages from Ergonode and saves them as Language entities in Shopware.'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $entities = $this->languageManager->syncLanguages($this->context);
+
+        if (empty($entities)) {
+            $io->info('No new languages created.');
+
+            return self::SUCCESS;
+        }
+
+        $io->success('Languages synchronized (Ergonode->Shopware).');
+        foreach ($entities as $entity => $ids) {
+            $io->success(["Created $entity:", ...$ids]);
+        }
+
+        return self::SUCCESS;
+    }
+}
