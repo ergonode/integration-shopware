1412e13a58833f0010253a39de10afd0292c11e3
Mariusz Piątek <mariusz.piatek@strix.net>
2022-07-13 14:27:21 +0200

    SWERG-61: Added plugin uninstall lifecycle manager

--- src/StrixErgonode.php
+++ src/StrixErgonode.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Strix\Ergonode;
 
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Strix\Ergonode\DependencyInjection\CompilerPass\GqlClientCacheCompilerPass;
+use Strix\Ergonode\Lifecycle\DatabaseLifecycleManager;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
@@ -18,5 +20,10 @@ class StrixErgonode extends Plugin
     {
         parent::build($container);
         $container->addCompilerPass(new GqlClientCacheCompilerPass());
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        DatabaseLifecycleManager::getInstance($this->container)->uninstall($uninstallContext);
     }
 }
