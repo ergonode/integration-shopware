d462276fb704cb6584a68179a48c85668a001481
Mariusz Piątek <mariusz.piatek@strix.net>
2022-07-13 13:32:05 +0200

    SWERG-57: Added test credentials endpoint

---
+++ src/Controller/Admin/TestCredentialsController.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Strix\Ergonode\Controller\Admin;
+
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Strix\Ergonode\Api\Client\HttpGqlClientFactory;
+use Strix\Ergonode\Api\ErgonodeAccessData;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @RouteScope(scopes={"administration"})
+ */
+class TestCredentialsController extends AbstractController
+{
+    private HttpGqlClientFactory $clientFactory;
+
+    public function __construct(HttpGqlClientFactory $clientFactory)
+    {
+        $this->clientFactory = $clientFactory;
+    }
+
+    /**
+     * @Route(
+     *     "/api/_action/strix/ergonode/test-credentials",
+     *     name="api.admin.strix.ergonode.test-credentials",
+     *     methods={"POST"},
+     *     defaults={"_route_scope"={"administration"}}
+     * )
+     */
+    public function testCredentials(RequestDataBag $dataBag): JsonResponse
+    {
+        $client = $this->clientFactory->create(
+            new ErgonodeAccessData(
+                (string)$dataBag->get('baseUrl'),
+                (string)$dataBag->get('apiKey')
+            )
+        );
+
+        $success = true;
+        try {
+            $client->runRawQuery('');
+        } catch (\Throwable $e) {
+            $success = false;
+        }
+
+        return new JsonResponse([
+            'success' => $success,
+        ]);
+    }
+}
