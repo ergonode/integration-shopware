a91ba0b9ae880379cf568d710137ac424c503d14
Filip Kowalik <filip.kowalik@strix.net>
2022-08-02 15:38:02 +0200

    SWERG-81: removed deprecated fields from gql queries

--- src/QueryBuilder/AttributeQueryBuilder.php
+++ src/QueryBuilder/AttributeQueryBuilder.php
@@ -41,11 +41,6 @@ class AttributeQueryBuilder
                                         'language',
                                         'value',
                                     ]),
-                                (new Query('placeholder'))
-                                    ->setSelectionSet([
-                                        'language',
-                                        'value',
-                                    ]),
                                 (new InlineFragment('DateAttribute'))
                                     ->setSelectionSet([
                                         new Query('code', AttributeTypesEnum::DATE),
