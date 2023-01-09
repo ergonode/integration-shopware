acaa0ff81d4ebd2093d337cd9a34d8b090c27624
dkozlowski <dawid.kozlowski@strix.net>
2023-01-09 15:07:37 +0100

    SWERG-203 comment to sleep() in CategoryTreeSyncProcessor

--- src/Processor/CategoryTreeSyncProcessor.php
+++ src/Processor/CategoryTreeSyncProcessor.php
@@ -118,6 +118,12 @@ class CategoryTreeSyncProcessor implements CategoryProcessorInterface
                 if (!$leafHasNextPage) {
                     $leafHasNextPage = $edge['node']['categoryTreeLeafList']['pageInfo']['hasNextPage'] ?? false;
                     $leafEndCursor = $edge['node']['categoryTreeLeafList']['pageInfo']['endCursor'] ?? null;
+                    // TODO:
+                    // Restore code removed in SWERG-174.
+                    // Temporary fix for SWERG-169. The issue is that removeOrphanedCategories adds 1 second to the
+                    // last sync time and when the sync is ran for the first time some trees can be processed under
+                    // 1 second resulting in them being completely removed because they have updated_at time before
+                    // the new lastSyncTime
                     sleep(2);
                 }
 
