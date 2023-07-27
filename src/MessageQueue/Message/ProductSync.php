f5fe5bcf1b8078efacdba2e209c5a8e326225f35
Tomasz Kardas <tomasz.kardas@strix.net>
2023-07-27 09:36:17 +0200

    SWER-83: default to asynchronous tasks

--- src/MessageQueue/Message/ProductSync.php
+++ src/MessageQueue/Message/ProductSync.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Ergonode\IntegrationShopware\MessageQueue\Message;
 
-class ProductSync
+use Shopware\Core\Framework\MessageQueue\AsyncMessageInterface;
+
+class ProductSync implements AsyncMessageInterface
 {
 }
