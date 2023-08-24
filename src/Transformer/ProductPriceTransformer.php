c9519570869c744104b28957df71e38898f56468
Tomasz Kardas <tomasz.kardas@strix.net>
2023-08-24 14:12:27 +0200

    wip

--- src/Transformer/ProductPriceTransformer.php
+++ src/Transformer/ProductPriceTransformer.php
@@ -8,43 +8,50 @@ use Ergonode\IntegrationShopware\DTO\ProductTransformationDTO;
 use Shopware\Core\Defaults;
 use Shopware\Core\Framework\Context;
 
-use function array_merge;
-
 class ProductPriceTransformer implements ProductDataTransformerInterface
 {
     public function transform(ProductTransformationDTO $productData, Context $context): ProductTransformationDTO
     {
         $swData = $productData->getShopwareData();
         $ergonodeData = $productData->getErgonodeData();
-        if(!$ergonodeData->getPriceNet() && !$ergonodeData->getPriceGross()) {
-            return $productData;
-        }
+        $defaultLanguage = $productData->getDefaultLanguage();
 
-        foreach($existingPrice->getElements() as $element) {
-            $elementArray = (array)$element;
-            var_dump(array_keys($elementArray));
-            //foreach($element as $row) {
-            //
-            //    var_dump(((array)$row)['currencyId']);
-            //}
-        }
-        //dump($existingPrice);
-        throw new \Exception('rpcei');
-        $price = $swData->getPrice();
-        $price = [
-            array_merge(
-                [
-                    'gross' => 0,
-                    'net' => 0,
-                    'linked' => false,
-                    'currencyId' => Defaults::CURRENCY,
-                ],
-                $price
-            ),
+        $pricePayload = [
+            'linked' => false,
+            'currencyId' => Defaults::CURRENCY,
         ];
-        $swData->setPrice($price);
+
+        if (!$productData->getSwProduct()?->getPrice()) {
+            $pricePayload = [
+                'gross' => (float)($ergonodeData->getPriceGross()?->getTranslation($defaultLanguage)?->getValue() ?? 0),
+                'net' => (float)($ergonodeData->getPriceNet()?->getTranslation($defaultLanguage)?->getValue() ?? 0),
+            ];
+        } else {
+            $pricePayload['gross'] = $ergonodeData->getPriceGross()
+                ? (float)$ergonodeData->getPriceGross()?->getTranslation($defaultLanguage)?->getValue()
+                : $this->getExistingGrossPrice($productData);
+            $pricePayload['net'] = $ergonodeData->getPriceNet()
+                ? (float)$ergonodeData->getPriceNet()?->getTranslation($defaultLanguage)?->getValue()
+                : $this->getExistingNetPrice($productData);
+        }
+        $swData->setPrice([$pricePayload]);
+
         $productData->setShopwareData($swData);
 
         return $productData;
+    }
+
+    private function getExistingGrossPrice(ProductTransformationDTO $productTransformationDTO): ?float
+    {
+        $price = $productTransformationDTO->getSwProduct()?->getPrice()->getCurrencyPrice(Defaults::CURRENCY);
+
+        return $price ? $price->getGross() : 0;
+    }
+
+    private function getExistingNetPrice(ProductTransformationDTO $productTransformationDTO): ?float
+    {
+        $price = $productTransformationDTO->getSwProduct()?->getPrice()->getCurrencyPrice(Defaults::CURRENCY);
+
+        return $price ? $price->getNet() : 0;
     }
 }
